ac8903a92d6519df7e93ec043ebdb25f38d9ae9b
kylebaugh <kylebaugh1@gmail.com>
2023-04-06 09:24:40 -0600

    testing

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '4125ee95fe4044b7938c08e254e05749',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.use(express.static(`${__dirname}/public`))
 
